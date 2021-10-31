d9fa02cdd4f526a33b0452090bde82884e5f1c0c
Vansh <thaparvansh29@gmail.com>
2021-10-31 22:05:11 +0530

    Replacing HTML files with PUG

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use('/admin',adminData.router)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.resolve('views/404.html'))
+    res.status(404).render('404.pug')
 }) 
 app.listen(3000)
