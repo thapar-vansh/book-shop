7c074188b9450c715ad80c82b9d10e0d83756778
Vansh <thaparvansh29@gmail.com>
2021-11-23 13:44:38 +0530

    Rendering view for a product by Id

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,9 +23,12 @@ let getProducts = (req, res, next) => {
 let getProduct = (req, res, next) => {
   const prodId = req.params.productId
   Product.findById(prodId, (product) => {
-    console.log(product)
+    res.render('shop/product-detail.ejs', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products',
+    })
   })
-  res.redirect('/')
 }
 
 let getCart = (req, res, next) => {
